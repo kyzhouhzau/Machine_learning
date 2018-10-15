61a22493f07a8171d81fae9566c40fe9aba086b1
Kaiyinzhou <33114591+kyzhouhzau@users.noreply.github.com>
2018-10-15 19:51:32 +0800

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -20,25 +20,24 @@ def build_data(n=200):
         noise = np.random.randn(1,1000)
         array1 = [k+part for part in noise]
         data.append(list(array1[0]))
-    return data
+    return data#生成(1,1000)的数据
 
 def pca(data,n_components):
-    meandata = np.mean(data,axis=0)
+    meandata = np.mean(data,axis=0)#竖着方向
     meanremoved = data-meandata
-    convdata = np.cov(meanremoved,rowvar=False)
-    enval,envec = np.linalg.eig(np.mat(convdata))
-    ensortind = np.argsort(enval)
-    enval = ensortind[:-(n_components+1):-1]
-    nedenvec = envec[:,enval]
-    lowdata = meanremoved*nedenvec
+    convdata = np.cov(meanremoved,rowvar=False)#计算协方差矩阵
+    enval,envec = np.linalg.eig(np.mat(convdata))#np.mat()将序列转为np的二维数组，np.linalg.eig返回特征值，和特征向量。
+    ensortind = np.argsort(enval)#排序并返回排序后的下标。
+    enval = ensortind[:-(n_components+1):-1]#反向选取index即选取最大的几个index
+    nedenvec = envec[:,enval]#取这几个特征值对应的特征向量。
+    lowdata = meanremoved*nedenvec#
     newdata = (lowdata*nedenvec.T)+meandata
-    return lowdata,newdata
+    return newdata
 
 data = build_data()
-lowdata,newdata = pca(data,2)
+newdata = pca(data,2)
 data = np.asarray(data)
 newdata = np.asarray(newdata)
-lowdata = np.asarray(lowdata)
 plt.scatter(data[:,0],data[:,1],marker='H',c='red')
 plt.scatter(newdata[:,0],newdata[:,1],marker='1',c='blue')
 
